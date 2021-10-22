a8f123fddc5d6612ff1d52a21ca4f459a2468ebe
agiglio6 <58085515+agiglio6@users.noreply.github.com>
2021-10-22 12:12:29 +0200

    Revert "Load data and split_data implemented"

--- src/fun_utils.py
+++ src/fun_utils.py
@@ -31,18 +31,4 @@ def split_data(x, y, tr_fraction=0.5):
     Split the data x, y into two random subsets
 
     """
-    n_samples = x.shape[0]
-    idx = np.linspace(0, n_samples - 1, num=n_samples, dtype=int)
-    np.random.shuffle(idx)
-
-    n_tr = int(n_samples * tr_fraction)
-    n_ts = int(n_samples - n_tr)
-
-    idx_tr = idx[:n_tr]
-    idx_ts = idx[n_tr:n_tr + n_ts]
-
-    x_tr = x[idx_tr, :]
-    y_tr = y[idx_tr]
-    x_ts = x[idx_ts, :]
-    y_ts = y[idx_ts]
-    return x_tr, y_tr, x_ts, y_ts
+    pass
