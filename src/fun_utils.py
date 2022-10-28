6b59857604364d2fd7e6f263c813ce3166dd4d7d
marko <m.ledda45@studenti.unica.it>
2022-10-28 10:37:00 +0200

    Split data method implemented

--- src/fun_utils.py
+++ src/fun_utils.py
@@ -31,4 +31,18 @@ def split_data(x, y, tr_fraction=0.5):
     Split the data x, y into two random subsets
 
     """
-    pass
+    num_samples = y.size
+    num_tr = int(tr_fraction * y.size)  # training
+    num_ts = num_samples - num_tr
+
+    tr_idx = np.zeros(num_samples, )  # [0, 0, 0, ..... 0]
+    tr_idx[0:num_tr] = 1  # this are the tr index
+    np.random.shuffle(tr_idx)  # randomize the vector
+
+    ytr = y[tr_idx == 1]  # this works in python, pass to ytr all the index of tr_idx that are equals to 1
+    yts = y[tr_idx == 0]
+
+    xtr = x[tr_idx == 1, :]  # these row, always all colums
+    xts = x[tr_idx == 0, :]
+
+    return xtr, ytr, xts, yts
