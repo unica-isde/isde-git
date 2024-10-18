f5f20a8a0ceb1312c42196164ad9f6750dedd0a5
Riccardo Deidda <127761846+RDbtx@users.noreply.github.com>
2024-10-18 17:32:45 +0200

    Update fun_utils.py

--- src/fun_utils.py
+++ src/fun_utils.py
@@ -1,4 +1,30 @@
 import numpy as np
+from pandas import read_csv
+
+
+
+def load_data(filename):
+    """
+    Load data from a csv file
+
+    Parameters
+    ----------
+    filename : string
+        Filename to be loaded.
+
+    Returns
+    -------
+    X : ndarray
+        the data matrix.
+
+    y : ndarray
+        the labels of each sample.
+    """
+    data = read_csv(filename)
+    z = np.array(data)
+    y = z[:, 0]
+    x = z[:, 1:]
+    return x, y
 
 
 def split_data(x, y, tr_frac=0.6):
