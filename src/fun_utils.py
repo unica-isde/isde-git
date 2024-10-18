3f2e2c77765489b18828c1d24928f3d4eefc9607
steve1234556 <134520986+steve1234556@users.noreply.github.com>
2024-10-18 16:25:42 +0200

    uploaded load_mnsit

--- src/fun_utils.py
+++ src/fun_utils.py
@@ -3,32 +3,42 @@ import numpy as np
 
 
 def load_data(filename):
-    """
-    Load data from a csv file
+	"""
+	Load data from a csv file
 
-    Parameters
-    ----------
-    filename : string
-        Filename to be loaded.
+	Parameters
+	----------
+	filename : string
+		Filename to be loaded.
 
-    Returns
-    -------
-    X : ndarray
-        the data matrix.
+	Returns
+	-------
+	X : ndarray
+		the data matrix.
 
-    y : ndarray
-        the labels of each sample.
-    """
-    data = read_csv(filename)
-    z = np.array(data)
-    y = z[:, 0]
-    X = z[:, 1:]
-    return X, y
+	y : ndarray
+		the labels of each sample.
+	"""
+	data = read_csv(filename)
+	z = np.array(data)
+	y = z[:, 0]
+	X = z[:, 1:]
+	return X, y
+
+def load_mnist_data(filename, n_samples=None):
+	"""This function returns MNIST handwritten digits and labels as ndarrays."""
+	data = pd.read_csv(filename)
+	data = np.array(data)  # cast pandas dataframe to numpy array
+	if n_samples is not None:  # only returning the first n_samples
+		data = data[:n_samples, :]
+	y = data[:,0]
+	x = data[:,1:] / 255.0
+	return x, y
 
 
 def split_data(x, y, tr_fraction=0.5):
-    """
-    Split the data x, y into two random subsets
+	"""
+	Split the data x, y into two random subsets
 
-    """
-    pass
+	"""
+	pass
