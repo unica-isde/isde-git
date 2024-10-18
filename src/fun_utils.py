d1af2e062d78c6280bf733553286ec5f9490e88c
Riccardo Deidda <riccardodeidda25@gmail.com>
2024-10-18 16:40:11 +0200

    split data function

--- src/fun_utils.py
+++ src/fun_utils.py
@@ -2,33 +2,22 @@ from pandas import read_csv
 import numpy as np
 
 
-def load_data(filename):
-    """
-    Load data from a csv file
+def split_data(x, y, tr_frac=0.6):
+    n_samples = x.shape[0]
 
-    Parameters
-    ----------
-    filename : string
-        Filename to be loaded.
+    n_tr = int(tr_frac * n_samples)
+    n_ts = n_samples - n_tr
 
-    Returns
-    -------
-    X : ndarray
-        the data matrix.
+    idx = np.linspace(0, n_samples, num=n_samples, endpoint=False, dtype='int')
+    np.random.shuffle(idx)
+    tr_idx = idx[:n_tr]
+    ts_idx = idx[n_tr:]
 
-    y : ndarray
-        the labels of each sample.
-    """
-    data = read_csv(filename)
-    z = np.array(data)
-    y = z[:, 0]
-    X = z[:, 1:]
-    return X, y
+    assert (n_tr == tr_idx.size)
+    assert (n_ts == ts_idx.size)
 
-
-def split_data(x, y, tr_fraction=0.5):
-    """
-    Split the data x, y into two random subsets
-
-    """
-    pass
+    xtr = x[tr_idx, :]
+    ytr = y[tr_idx]
+    xts = x[ts_idx, :]
+    yts = y[ts_idx]
+    return xtr, ytr, xts, yts,
