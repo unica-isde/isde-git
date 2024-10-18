7dbfcc651322f1ba344f1a6eb901a9f12cc1d07e
Giulia <giulia.melis0401@gmail.com>
2024-10-18 17:27:00 +0200

    Split function implementation

--- src/fun_utils.py
+++ src/fun_utils.py
@@ -1,5 +1,5 @@
-from pandas import read_csv
 import numpy as np
+from pandas import read_csv
 
 
 def load_data(filename):
@@ -25,10 +25,26 @@ def load_data(filename):
     X = z[:, 1:]
     return X, y
 
-
 def split_data(x, y, tr_fraction=0.5):
-    """
-    Split the data x, y into two random subsets
 
-    """
-    pass
+        """Split data to create a random tr-ts partition."""
+        n, d = x.shape
+
+        # check if y and x have a consistent no. of samples and labels
+        n1 = y.size
+        assert (n == n1)
+
+        n_tr = int(np.round(n * tr_fraction))
+
+        idx = np.array(range(0, n))  # 0, 1, 2, ..., n-1
+        np.random.shuffle(idx)
+        idx_tr = idx[0:n_tr]
+        idx_ts = idx[n_tr:n]
+
+        xtr = x[idx_tr, :]
+        ytr = y[idx_tr]
+        xts = x[idx_ts, :]
+        yts = y[idx_ts]
+
+        return xtr, ytr, xts, yts
+
